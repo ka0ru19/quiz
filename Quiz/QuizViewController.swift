ca440f310d0f7fae1ea21f8929862269033a25bc
井上航 <k.prfm19@gmail.com>
2015-06-10 18:49:01 +0900

    first commit

--- Quiz/QuizViewController.swift
+++ Quiz/QuizViewController.swift
@@ -11,73 +11,141 @@ import UIKit
 class QuizViewController: UIViewController {
     //出題数
     var questionNumber:Int = 5
-    //何問正解したかの共有
-    var correntAnswer:Int = 0
-    //randomの共有
-    var random:Int = 0
-    //解いた問題数の共有
+    
+    //現在の問題数
     var sum:Int = 0
-    //Quiz配列
-    var quiz=[NSArray]()
-    //weak参照
-    @IBOutlet weak var QuizText: UITextView!
+    
+    //正解数
+    var correctAnswer:Int = 0
+    
+    //乱数
+    var random:Int = 0
+    
+    //クイズを格納する配列
+    var quizArray = [NSMutableArray]()
+    
+    //クイズを表示するTextView
+    @IBOutlet var quizTextView: UITextView!
+    
+    //選択肢のボタン
+    @IBOutlet var choiceButtons: Array<UIButton>!
 
-    override func viewDidLoad() {
+    //正誤判定の画像（マルバツ）
+    @IBOutlet var AnswerMark: UIImageView!
+    let AnswerTrue:UIImage!  = UIImage(named: "true.png")
+    let AnswerFalse:UIImage! = UIImage(named: "false.png")
+    //var AnsAnimeArray = [NSArray]()
+    var AnsTrueAnimeArray : Array<UIImage> = []
+    var AnsFalseAnimeArray : Array<UIImage> = []
+    
+    
+    override func viewDidLoad() { //アプリ起動後の最初の処理
         super.viewDidLoad()
         
-        //------------------------ここから下にクイズを書く------------------------//
-        quiz.append(["問題文","選択肢","選択肢2","選択肢3",1])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
-        quiz.append(["問題文2","選択肢","選択肢2","選択肢3",2])
+        AnsTrueAnimeArray.append(AnswerTrue)
+        AnsFalseAnimeArray.append(AnswerFalse)
+        AnswerMark.image = nil
         
+        //添字[5]は問題番号を示す検番
         //------------------------ここから下にクイズを書く------------------------//
+        quizArray.append(["問題文1 都道府県の数は？","47","51","53",1,1])
+        quizArray.append(["問題文2 孫さん","au","docomo","softbank",3,2])
+        quizArray.append(["問題文3 Steve Jobs","Apple","Microsoft","SONY",1,3])
+        quizArray.append(["問題文4 iOSのプログラミング言語","Java","Ruby","Swift",3,4])
+        quizArray.append(["問題文5 石の上にも","ぼたもち","三年","蜂",2,5])
+        quizArray.append(["問題文6 M-T-W-T-?-S-S","F","K","R",1,6])
 
-        random = Int(arc4random()%UInt32(quiz.count))
-        QuizText.text=quiz[random][0] as NSString
+        //------------------------ここから下にクイズを書く------------------------//
+        choiceQuiz()  //シャッフルする
+    }
+    
+    func choiceQuiz() {
+        //クイズの問題文をシャッフルしてTextViewにセット
+        //乱数の代入
+        if quizArray.count==0 {
+            random = 0
+        }else{
+        random = Int(arc4random_uniform(UInt32(quizArray.count)))
+        }
+        //問題文(quizArray[random][0])をテキストラベルに代入
+        quizTextView.text = quizArray[random][0] as NSString
         
+        //選択肢のボタンにそれぞれ選択肢のテキストをセット
+        for var i = 0; i < choiceButtons.count; i++ {
+            choiceButtons[i].setTitle(quizArray[random][i+1] as NSString, forState: .Normal)
+            
+            //どのボタンが押されたか判別するためのtagをセット
+            choiceButtons[i].tag = i + 1;
+            
+        }
     }
-    @IBAction func Select1() {
+    
+    @IBAction func choiceAnswer(sender: UIButton) {
         sum++
-        if 1==quiz[random][4] as NSObject{
-            current()
+        print("\(sum)問目の問題( 残り\(questionNumber-sum+1)問 ), ")
+        println("random　-> \(random)")
+        print("\(sum)番目の問題(Q\(quizArray[random][5] as Int)) は \(sender.tag)番目の選択肢を選択 ")
+        AnswerMark.animationRepeatCount = 10; //n回
+        AnswerMark.animationDuration = 5; //0.8秒に設定
+        if quizArray[random][4] as Int == sender.tag {
+            //正解数を増やす
+            correctAnswer++
+            println("-> 正解")
+            AnswerMark.animationImages = AnsTrueAnimeArray
+        }else{
+            println("-> 不正解")
+            AnswerMark.animationImages = AnsFalseAnimeArray
         }
-        if(sum == questionNumber){
-            tapBtn()
+        // アニメーションを開始
+        AnswerMark.startAnimating()
+        let delay = 0.6 * Double(NSEC_PER_SEC) //delay秒で正誤マークの表示を終了
+        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+        dispatch_after(time, dispatch_get_main_queue()) { //マーク表示終了後の処理
+            self.AnswerMark.stopAnimating() //表示終了
+            self.AnswerMark.image = nil
+            
+            //解いた問題数の合計が予め設定していた問題数に達したら
+            if self.sum == self.questionNumber {
+                self.performSegueToResult()  //結果画面へ
+            }else{
+                self.quizArray.removeAtIndex(self.random)  //添字[random]の問題を排除
+                self.choiceQuiz() //次の問題を選出
+            }
+
         }
+        
+        
+        println("正答数:\(correctAnswer)\n")
+        /*
+        //解いた問題数の合計が予め設定していた問題数に達したら
+        if sum == questionNumber {
+            performSegueToResult()  //結果画面へ
+        }else{
+        quizArray.removeAtIndex(random)  //添字[random]の問題を排除
+        choiceQuiz()
+        }
+*/
     }
-    @IBAction func Select2() {
-        sum++
-        if 2==quiz[random][4] as NSObject {
-            current()
-        }
-        if sum == questionNumber{
-            tapBtn()
-        }
-    }
-    @IBAction func Select3() {
-        sum++
-        if 3==quiz[random][4] as NSObject {
-            current()
-        }
-        if sum == questionNumber{
-            tapBtn()
-        }
-    }
-    func current(){
-        println("true")
-        correntAnswer++
-    }
-    func tapBtn() {
+    
+    func performSegueToResult() {
         performSegueWithIdentifier("toResultView", sender: nil)
     }
-/*
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "toResultView") {
+            
+            var ResultView : ResultViewController = segue.destinationViewController as ResultViewController
+
+            ResultView.questionNumber = self.questionNumber
+            ResultView.correctAnswer  = self.correctAnswer  //ResultViewのcorrectAnswerにcorrectAnswerを代入
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-*/
+    
 }
+
+
--- Quiz/ResultViewController.swift
+++ Quiz/ResultViewController.swift
@@ -9,10 +9,42 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    var questionNumber:Int = 0
+    var correctAnswer:Int = 0
+    @IBOutlet weak var ResultLabel: UILabel!
+    
+    @IBOutlet weak var TextMessege: UITextView!
+    
+    @IBOutlet weak var ResultImage: UIImageView!
+    let ResultPic0: UIImage! = UIImage(named: "God01.png")
+    let ResultPic1: UIImage! = UIImage(named: "human01.png")
+    let ResultPic2: UIImage! = UIImage(named: "monkey01.png")
+    let ResultPic3: UIImage! = UIImage(named: "horse01.png")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //pointは正答率(0.0~1.0)
+        var point :Double = Double(correctAnswer) / Double(questionNumber)
+        
+        println("Result画面: 正答数/問題数 -> \(correctAnswer)/\(questionNumber), 正答率 -> \(point*100)%")
+        ResultLabel.text = String("正答率 -> \(point*100)%")
+        
+        if point == 1 {
+            TextMessege.text = String("全問正解！\nおめでとうございます！\nあなたの常識度は神レベルです。")
+            ResultImage.image = ResultPic0
+        } else if point > 0.7 {
+            TextMessege.text = String("あなたの常識度は人間レベルです。")
+            ResultImage.image = ResultPic1
+        } else if point > 0.3 {
+            TextMessege.text = String("あなたは常識度は猿レベルです。")
+            ResultImage.image = ResultPic2
+        } else {
+            TextMessege.text = String("あなたは常識度は馬レベルです。\n頑張りましょう。")
+            ResultImage.image = ResultPic3
+        }
+        
+        
         // Do any additional setup after loading the view.
     }
 
@@ -20,16 +52,5 @@ class ResultViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
